fb07370b06c7e50c932fb5c31ab03c1fb1dffe65
candynet <133592778+candynet@users.noreply.github.com>
2024-06-16 16:19:56 +0800

    Add files via upload

---
+++ 派蒙病毒/Paimon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PaimonVirus
+{
+    public partial class Paimon : Form
+    {
+        private int x;
+
+        private int y;
+        
+        public Paimon(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Paimon_Load(object sender, EventArgs e)
+        {
+            this.Left = x;
+            this.Top = y;
+            new Thread(() =>
+            {
+                Thread.Sleep(2000);
+                this.Close();
+            }).Start();
+        }
+    }
+}
